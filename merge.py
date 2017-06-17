0db838d902b45e1e0524d553192114e229805633
rodlin <im.rod@outlook.com>
2017-06-17 10:15:28 +0800

    170617#01: update

--- merge.py
+++ merge.py
@@ -13,32 +13,43 @@ def expand(path):
 	src_reg = re.compile(r"src=['\"]([^'\"]*)['\"]")
 
 	css_reg = re.compile(r"<link[^>]*>")
-	script_reg = re.compile(r"<script[^>]*>[^<]*</script>");
+	script_reg = re.compile(r"<script[^>]*>[^<]*</script>")
 
-	base = os.path.dirname(path);
+	base = os.path.dirname(path)
+	abs_base = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 	def rep_css(m):
 		tag = m.group(0)
-		href = href_reg.search(tag);
+		href = href_reg.search(tag)
 
 		if href:
 			href = href.group(1)
 
 			if href[:4] != "http":
-				with open(base + href) as f:
+				if href[0] == "/":
+					path = abs_base + href
+				else:
+					path = base + href
+
+				with open(path) as f:
 					return "<style>" + f.read() + "</style>"
 
 		return tag
 
 	def rep_js(m):
 		tag = m.group(0)
-		js = src_reg.search(tag);
+		js = src_reg.search(tag)
 
 		if js:
 			js = js.group(1)
 
 			if js[:4] != "http":
-				with open(base + js) as f:
+				if js[0] == "/":
+					path = abs_base + js
+				else:
+					path = base + js
+
+				with open(path) as f:
 					return "<script>" + f.read() + "</script>"
 
 		return tag

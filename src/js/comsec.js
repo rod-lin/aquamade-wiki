e4c5d4922b09dd1d8911c23ae0487ef4a319c4d0
rodlin <im.rod@outlook.com>
2017-11-01 18:21:46 +0800

    011117#03: update

--- src/js/comsec.js
+++ src/js/comsec.js
@@ -1,52 +1,64 @@
 $(function () {
-    
-    function bindSticky() {
-        $(".comsec-main .float-menu").sticky({
-            scrollContext: $(".main.cont-wrap"),
-            context: $(".comsec-main"),
-            offset: 100,
-            observeChanges: true
-        });
-    }
+	
+	function bindSticky(first) {
+		$(".comsec-main .float-menu").sticky("destroy").sticky({
+			scrollContext: $(".main.cont-wrap"),
+			context: $(".comsec-main"),
+			offset: 100,
 
-    setTimeout(bindSticky, 1000);
-    
-    function showPassage(name) {
-        $(".comsec-main .passage.shown").removeClass("shown");
-        $(".comsec-main .passage.passage-" + name).addClass("shown");
+			onStick: function () {
+				$(".comsec-main .float-menu").css("display", "");
+			},
 
-        setTimeout(function () {
-            if (window.imgview) {
-                imgview.refresh();
-            }
+			onBottom: function () {
+				$(".comsec-main .float-menu").css("display", "none");
+			}
+		});
+	}
 
-            bindSticky();
-        }, 100);
-    }
-    
-    function setUniqueActive(dom) {
-        $(dom).siblings(".active").removeClass("active");
-        $(dom).addClass("active");
-    }
-    
-    $(".comsec-main .passlink").each(function (i, dom) {
-        dom = $(dom);
-        
-        dom.click(function () {
-            var passage = dom.data("passage"); 
-            
-            if (passage)
-                showPassage(passage);
-        
-            setUniqueActive(dom);
-            
-            $(".comsec-main .passlink").each(function (i, dom) {
-                dom = $(dom);
-                
-                if (dom.data("passage") == passage) {
-                    setUniqueActive(dom);
-                }
-            });
-        });
-    });
+	setTimeout(function () {
+		bindSticky(true);
+	}, 1000);
+	
+	function showPassage(name) {
+		$(".comsec-main .passage.shown").removeClass("shown");
+		$(".comsec-main .passage.passage-" + name).addClass("shown");
+
+		var top = $(".main.cont-wrap").scrollTop();
+		gradScroll(top + $(".comsec-main .passage.shown").offset().top - 115);
+
+		setTimeout(function () {
+			if (window.imgview) {
+				imgview.refresh();
+			}
+
+			bindSticky();
+		}, 100);
+	}
+	
+	function setUniqueActive(dom) {
+		$(dom).siblings(".active").removeClass("active");
+		$(dom).addClass("active");
+	}
+	
+	$(".comsec-main .passlink").each(function (i, dom) {
+		dom = $(dom);
+		
+		dom.click(function () {
+			var passage = dom.data("passage"); 
+			
+			if (passage)
+				showPassage(passage);
+		
+			setUniqueActive(dom);
+			
+			$(".comsec-main .passlink").each(function (i, dom) {
+				dom = $(dom);
+				
+				if (dom.data("passage") == passage) {
+					setUniqueActive(dom);
+				}
+			});
+		});
+	});
 });
